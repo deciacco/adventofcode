33a3f463bea041fb8c9667495befda42a84efa7a
deciacco <ec@deciacco.com>
2022-02-02 22:29:38 -0600

    day 5 start

---
+++ 2021/day05/day5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "|)ehko"     
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+# Load libraries
+import os
+import sys
+
+# Constants
+OWNP = os.path.abspath(os.path.dirname(sys.argv[0])) # Own path
+FILENM = '/input.txt'
+
+
+
+
+# Main
+def main():
+    print('ok')
+
+
+
+
+
+
+
+# Start
+if __name__ == "__main__":
+    main()
